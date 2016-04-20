27dd4f0ed3db67bd5835ac7ab88c4c96a154748a
Jarrod Taylor <jarrodtaylor@icloud.com>
2016-04-20 16:18:34 -0400

    Update 2016-04-20T20:18:29.307Z

---
+++ app.js
@@ -0,0 +1,48 @@
+var classify, filterPosts, toggleNav;
+
+classify = function() {
+  var path;
+  path = window.location.pathname.split('/')[1];
+  return document.getElementsByTagName('body')[0].className = path || 'home';
+};
+
+toggleNav = function(event) {
+  var nav;
+  event.preventDefault();
+  nav = document.getElementsByTagName('nav')[0];
+  if (nav.className === 'show') {
+    return nav.className = '';
+  } else {
+    return nav.className = 'show';
+  }
+};
+
+filterPosts = function() {
+  var i, j, len, len1, post, posts, query, results, results1;
+  console.log('filtering');
+  query = document.getElementById('search').value.toLowerCase();
+  posts = document.getElementsByClassName('searchable');
+  if (query.length > 0) {
+    results = [];
+    for (i = 0, len = posts.length; i < len; i++) {
+      post = posts[i];
+      results.push((function(post) {
+        if (post.children[1].innerHTML.toLowerCase().indexOf(query) > -1) {
+          return post.style.display = 'block';
+        } else {
+          return post.style.display = 'none';
+        }
+      })(post));
+    }
+    return results;
+  } else {
+    results1 = [];
+    for (j = 0, len1 = posts.length; j < len1; j++) {
+      post = posts[j];
+      results1.push((function(post) {
+        return post.style.display = 'block';
+      })(post));
+    }
+    return results1;
+  }
+};
